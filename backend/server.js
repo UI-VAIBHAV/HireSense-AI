2237158b97aafbf2d104bf9fb3e00099294faa9b
UI-VAIBHAV <vaibhavpandey9125@gmail.com>
2026-06-20 03:29:09 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://hire-sense-ai-drab.vercel.app"
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -27,7 +30,10 @@ connectdb();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://hire-sense-ai-drab.vercel.app"
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   })
