12cafc81a8d98ed73ec04bdefa3c0bf6b36ee5fd
UI-VAIBHAV <vaibhavpandey9125@gmail.com>
2026-06-20 03:34:05 +0530

    Fix Vercel routing

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://hire-sense-ai-drab.vercel.app"
+      "https://hire-sense-ai-drab.vercel.app/login"
     ],
     methods: ["GET", "POST"],
     credentials: true,
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://hire-sense-ai-drab.vercel.app"
+      "https://hire-sense-ai-drab.vercel.app/login"
     ],
     methods: ["GET", "POST"],
     credentials: true,
